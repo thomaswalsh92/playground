4bcac46161fc16839396352ba685871d188a2f01
Thomas Walsh <thomaseawalsh@gmail.com>
2022-11-29 22:26:12 +0000

    Added isometric camera

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,50 @@
 import * as THREE from "three";
-import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import {
+  BoxGeometry,
+  Light,
+  Mesh,
+  MeshBasicMaterial,
+  MeshLambertMaterial,
+  OrthographicCamera,
+  PerspectiveCamera,
+  PointLight,
+  Scene,
+  SphereGeometry,
+  WebGLRenderer,
+} from "three";
 
 const scene = new Scene();
-const camera = new PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
+const box = new BoxGeometry(1, 1, 1);
+const material = new MeshLambertMaterial({ color: "0xffffff" });
+const mesh = new Mesh(box, material);
+const aspect = window.innerWidth / window.innerHeight;
+const d = 5;
+const camera = new THREE.OrthographicCamera(
+  -d * aspect,
+  d * aspect,
+  d,
+  -d,
+  1,
   1000
 );
+const light = new PointLight(0xff00ff, 10, 100);
+light.position.set(0, 1, 10);
 
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor("#e5e5e5");
 document.body.appendChild(renderer.domElement);
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.updateProjectionMatrix();
+});
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xf0ffff });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+scene.add(mesh);
+scene.add(light);
 
-camera.position.z = 5;
-
+camera.position.set(20, 20, 20); // all components equal
+camera.lookAt(scene.position); // or the origin
 const animate = () => {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 };
