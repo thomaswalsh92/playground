3f010d9cc7aa8be2398f8d81aaee37edb0f826ff
Thomas Walsh <thomaseawalsh@gmail.com>
2022-11-29 20:56:44 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = () => {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+
+animate();
