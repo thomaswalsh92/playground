4ad7e1b5049614fd2bb019d4a95b1b3d7b1d5a59
Thomas Walsh <thomaseawalsh@gmail.com>
2022-11-29 21:04:02 +0000

    Test gitignore

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const material = new THREE.MeshBasicMaterial({ color: 0xf0ffff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
